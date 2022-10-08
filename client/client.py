2022c157ff726a1fd025356d9cae97cc5260dbfb
semyon <supergamer170302@gmail.com>
2022-10-08 23:56:45 +0000

    backend-api

---
+++ client/client.py
@@ -0,0 +1,14 @@
+import requests
+import json
+  
+URL = "http://127.0.0.1:8002" #can be changed on demand 
+headers = {'Content-type': 'application/json'}
+
+if __name__ == '__main__':
+    data = {
+        'category': 'BUH',
+        'timestampStart': 1,
+        'timestampEnd': 50
+    }
+    r = requests.get(url = (URL + '/get_digest'), headers = headers, data = json.dumps(data))
+    print(r.json())
